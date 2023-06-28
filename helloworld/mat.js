9ca8c18bf734498d32119545fc8dc98aac9efffd
Victron012 <victron.dafonte@gmail.com>
2023-06-28 16:37:45 -0300

    Primeira aula do curso

---
+++ helloworld/mat.js
@@ -0,0 +1,8 @@
+var num = 10
+var num2 = 20
+
+function Somar(a,b){
+    return a + b;
+}
+
+console.log(Somar(num, num2))
---
+++ helloworld/nome.js
@@ -0,0 +1,4 @@
+var nome = "Victor"
+var sobrenome = "Santana"
+
+console.log(nome + " " +sobrenome)
