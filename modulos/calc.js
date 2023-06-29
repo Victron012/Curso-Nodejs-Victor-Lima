2fef39523b32cb57faae47174df690e6553bac01
Victron012 <victron.dafonte@gmail.com>
2023-06-29 10:53:15 -0300

    Módulos

---
+++ modulos/calc.js
@@ -0,0 +1,9 @@
+var somaFunc = require("./funcoes/soma");
+var subFunc = require("./funcoes/sub");
+var multiFunc = require("./funcoes/multi");
+var divFunc = require("./funcoes/div");
+
+console.log("Soma: "+somaFunc(1,2))
+console.log("Sutração: "+subFunc(10,5))
+console.log("Multiplicação: "+multiFunc(100,10))
+console.log("Divisão: "+divFunc(48,3))
